e40fbaca0173827a77f1155b929bca0f985c567d
sabaslote <112454708+sabaslote@users.noreply.github.com>
2022-11-22 19:57:39 -0600

    adding shape to map

--- js/map.js
+++ js/map.js
@@ -1,17 +1,20 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 35.8617, lng: 104.1954 },
-    zoom: 8,
+    zoom: 10,
   });
   var marker = new google.maps.Marker({
     position: { lat: 35.8617, lng: 104.1954 },
     map: map,
   });
-  var circleArea = new google.maps.Circle({
-    position: { lat: 35.8617, lng: 104.1954 },
-	radius:100,
-	stroke0pacity:0.8,
-	fillColor:"rgba(0,0,255,0.4)",
+  var addCircle = new google.maps.Circle({
+    center: { lat: 35.8617, lng: 104.1954 },
+	radius:1000,
+    strokeColor: "#FF0000",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#FF0000",
+    fillOpacity: 0.35,
 	map: map,
   });
 }
