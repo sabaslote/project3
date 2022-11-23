183945871edd6da4166a319ee8802afb4232e8c9
sabaslote <112454708+sabaslote@users.noreply.github.com>
2022-11-22 19:46:01 -0600

    adding css to map id

--- js/map.js
+++ js/map.js
@@ -1,8 +1,18 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: 35.8617, lng: 104.1954 },
     zoom: 8,
   });
+  var marker = new google.maps.Marker({
+    position: { lat: 35.8617, lng: 104.1954 },
+    map: map,
+  });
+  var circleArea = new google.maps.Circle({
+    position: { lat: 35.8617, lng: 104.1954 },
+	radius:100,
+	stroke0pacity:0.8,
+	fillColor:"rgba(0,0,255,0.4)",
+	map: map,
+  });
 }
-
 window.initMap = initMap;
